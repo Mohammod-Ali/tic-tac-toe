bfecc554ed17cd38c0fec985033242982761f806
Mohammod Ali <mohammodali651@gmail.com>
2024-06-24 03:24:06 +0600

    tic tac toe board added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,46 @@
-import './App.css'
+import { useState } from "react";
 
-function App() {
+function Square({value}) {
+
+  
+
+  function handleClick () {
+    
+  }
+
+  
+  return (
+    <button onClick={handleClick} className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">
+      {value}
+    </button>
+  );
+}
+
+
+
+function Board() {
+
+  const [ Squares, setSquares] = useState(Array(9).fill(null))
 
   return (
     <>
-     <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <div className="flex">
+        <Square value={Squares[0]}></Square>
+        <Square value={Squares[1]}></Square>
+        <Square value={Squares[2]}></Square>
+      </div>
+      <div className="flex">
+        <Square value={Squares[3]}></Square>
+        <Square value={Squares[4]}></Square>
+        <Square value={Squares[5]}></Square>
+      </div>
+      <div className="flex">
+        <Square value={Squares[6]}></Square>
+        <Square value={Squares[7]}></Square>
+        <Square value={Squares[8]}></Square>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default Board;
